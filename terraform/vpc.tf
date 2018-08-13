8cb7793b7f5ae3b3bf044172106006032fa8918d
nalbam <me@nalbam.com>
2018-08-13 11:16:32 +0900

    azs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,9 +7,9 @@ module "vpc" {
   name = "vpc-VPC-NAME"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["ap-northeast-2a","ap-northeast-2c"]
+  private_subnets = ["10.0.11.0/24","10.0.12.0/24"]
+  public_subnets  = ["10.0.21.0/24","10.0.22.0/24"]
 
   enable_nat_gateway = true
 #   single_nat_gateway = false
