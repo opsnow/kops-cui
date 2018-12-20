6cb8dbb6da7c419f0ae09cc067982200ef353753
jamje.kim <jamje.kim@bespinglobal.com>
2018-12-20 10:47:21 +0900

    Backup helm chart version to YOUR_DIR fix

--- backup-ver.sh
+++ backup-ver.sh
@@ -2,6 +2,14 @@
 
 SHELL_DIR=$(dirname $0)
 
+if [ ! -f $SHELL_DIR/versions ]; then
+    echo "Need a versions file"
+    exit 0
+fi
+
+. ${SHELL_DIR}/common.sh
+. ${SHELL_DIR}/default.sh
+
 BACKUP_DIR=$1
 
 if [ -z $BACKUP_DIR ]; then
@@ -17,39 +25,32 @@ CHART_LIST=$(cat $SHELL_DIR/versions)
 # 한줄씩 읽어서
 # 설치된 버전 ver1
 # yaml 파일을 찾아서
-# yaml 에 명시된 버전 ver2
-# ver2 를 ver1 로 replace
+# ver1 로 replace
 while read LINE; do
 
     # get chart name
-	chart_name=$(echo $LINE | awk '{print $1}')
+    chart_name=$(echo $LINE | awk '{print $1}')
 
     # get_build_ver
     ver1=$(echo $LINE | awk '{print $2}'| rev | cut -d'-' -f1 | rev)
 
     # confirm chart version
-    echo "BUILD : " $chart_name $ver1
+    echo "BUILD VER : " $chart_name $ver1
 
     #find file
     file=$(find $BACKUP_DIR -name "$chart_name.yaml")
 
-    echo "FILE  : " $file
+    echo "FILE      : " $file
 
     if [ -z $file ]; then
         # no file
+        echo "Not found file : $file"
         continue;
     fi
     # found file
-    # get_input_ver
-    ver2=$(cat $file | grep chart-version | awk '{print $3}')
-
-    echo "YAML  : " $ver2
-
     prefix="# chart-version: "
-    if [ $ver2 == "latest" ]; then
-        echo "CMD   : " "sed -i -e 's/$prefix$ver2/$prefix$ver1/g' $file"
-        sed -i -e "s/$prefix$ver2/$prefix$ver1/g" $file
-    fi
+    echo "REPLACE   : " "$prefix$ver1"
+    _replace "s/$prefix.*/$prefix$ver1/g" $file
 
     echo "============================="
 	
