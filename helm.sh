b0b49f8c48567bc5173112e9f754ebe38f0b86fd
Jamje Kim <jamje.kim@bespinglobal.com>
2019-02-22 15:25:53 +0900

    Mount exist EFS for Restore

--- helm.sh
+++ helm.sh
@@ -905,6 +905,9 @@ efs_create() {
     # echo "Security group for mount targets:"
     _result "EFS_SG_ID=${EFS_SG_ID}"
 
+    question "Would you like to mount exist EFS? Input your file system id.(Format: fs-00011122) : "
+    EFS_ID=${ANSWER}
+
     if [ -z ${EFS_ID} ]; then
         # create an efs
         EFS_LENGTH=$(aws efs describe-file-systems --creation-token ${CLUSTER_NAME} | jq '.FileSystems | length')
